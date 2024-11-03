4bf01aebc7fb3e6bf4ba66cd4a13ab1cbb5eeff6
Nick Cuneo <69825418+nicholascuneo@users.noreply.github.com>
2024-11-03 06:48:26 -0800

    Move ranks list to create deck function
    Add parameter to create_game_deck to allow input for number of
    standard decks used in game. Moved general ranks list to inside
    of function allowing removal of ranks parameter.

--- game.py
+++ game.py
@@ -6,7 +6,9 @@ def generate_suit(card_ranks, suit_symbol):
     return [rank + suit_symbol for rank in card_ranks]
 
 # Create main gameplay deck based on player preferred number of standard decks
-def create_game_deck(card_ranks, num_decks=1):
+def create_game_deck(num_decks=2):
+    ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
     # Generate all suits for a single deck
     clubs = generate_suit(ranks, 'c')
     diamonds = generate_suit(ranks, 'd')
@@ -21,7 +23,10 @@ def create_game_deck(card_ranks, num_decks=1):
 
     return game_deck
 
-#for card in face_cards:
-#    clubs.append(card + 'c')
+num_decks = int(input("How many decks would you like to play with? "))
 
-ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K' , 'A']
+game_deck = create_game_deck(num_decks)
+
+#print(game_deck)
+#print("Game deck created with {} decks of cards".format(num_decks))
+#print("Game deck created with {} number of cards".format(len(game_deck)))
